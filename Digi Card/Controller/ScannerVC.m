6f2d339c566a5d1ca2216441d42c239a7253c251
UMENIT <umenit@Ajays-Mac-mini.local>
2017-12-07 17:35:41 +0530

    Added google API

--- Digi Card/Controller/ScannerVC.m
+++ Digi Card/Controller/ScannerVC.m
@@ -1749,6 +1749,98 @@
     [[APPDELEGATE.maintab.viewControllers objectAtIndex:0] popToRootViewControllerAnimated:YES];
 }
 
+- (UIImage *) resizeImage: (UIImage*) image toSize: (CGSize)newSize {
+    UIGraphicsBeginImageContext(newSize);
+    [image drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newImage;
+}
+
+- (NSString *) base64EncodeImage: (UIImage*)image {
+    NSData *imagedata = UIImagePNGRepresentation(image);
+    
+    // Resize the image if it exceeds the 2MB API limit
+    if ([imagedata length] > 2097152) {
+        CGSize oldSize = [image size];
+        CGSize newSize = CGSizeMake(800, oldSize.height / oldSize.width * 800);
+        image = [self resizeImage: image toSize: newSize];
+        imagedata = UIImagePNGRepresentation(image);
+    }
+    
+    NSString *base64String = [imagedata base64EncodedStringWithOptions:NSDataBase64EncodingEndLineWithCarriageReturn];
+    return base64String;
+}
+
+- (void) createRequest: (NSString*)imageData {
+    // Create our request URL
+    
+    NSString *urlString = @"https://vision.googleapis.com/v1/images:annotate?key=";
+    NSString *API_KEY = @"AIzaSyDEAOyeLf6rEVrHU75I4S_l5Gyuu1Qm1yU";
+    
+    NSString *requestString = [NSString stringWithFormat:@"%@%@", urlString, API_KEY];
+    
+    NSURL *url = [NSURL URLWithString: requestString];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    [request setHTTPMethod: @"POST"];
+    [request addValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    [request
+     addValue:[[NSBundle mainBundle] bundleIdentifier]
+     forHTTPHeaderField:@"X-Ios-Bundle-Identifier"];
+    
+    // Build our API request
+    NSDictionary *paramsDictionary =
+    @{@"requests":@[
+              @{@"image":
+                    @{@"content":imageData},
+                @"features":@[
+                        @{@"type":@"TEXT_DETECTION"}]}]};
+    
+    NSError *error;
+    NSData *requestData = [NSJSONSerialization dataWithJSONObject:paramsDictionary options:0 error:&error];
+    [request setHTTPBody: requestData];
+    
+    // Run the request on a background thread
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        [self runRequestOnBackgroundThread: request];
+    });
+}
+
+- (void)runRequestOnBackgroundThread: (NSMutableURLRequest*) request {
+    NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^ (NSData *data, NSURLResponse *response, NSError *error) {
+        [self analyzeResults:data];
+    }];
+    [task resume];
+}
+
+- (void)analyzeResults: (NSData*)dataToParse {
+    
+    // Update UI on the main thread
+    dispatch_async(dispatch_get_main_queue(), ^{
+        
+        NSError *e = nil;
+        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:dataToParse options:kNilOptions error:&e];
+        
+        NSArray *responses = [json objectForKey:@"responses"];
+        NSLog(@"%@", responses);
+        NSDictionary *responseData = [responses objectAtIndex: 0];
+        NSDictionary *errorObj = [json objectForKey:@"error"];
+        
+        // Check for errors
+        if (errorObj) {
+            NSString *errorString1 = @"Error code ";
+            NSString *errorCode = [errorObj[@"code"] stringValue];
+            NSString *errorString2 = @": ";
+            NSString *errorMsg = errorObj[@"message"];
+            NSString *errorStr = [NSString stringWithFormat:@"%@%@%@%@", errorString1, errorCode, errorString2, errorMsg];
+            NSLog(@"%@",errorStr);
+        } else {
+            NSDictionary *labelFullTextAnnotations = [responseData objectForKey:@"fullTextAnnotation"];
+            NSLog(@"%@",[labelFullTextAnnotations objectForKey:@"text"]);
+        }
+    });
+    
+}
 
 
 @end
